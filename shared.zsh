885a08af23ae45604e47e697be06a12fde67b7e3
Ilya Kulakov <kulakov.ilya@gmail.com>
2017-01-05 13:34:07 -0800

    Env: export LC_CTYPE and set SH_WORD_SPLIT
    Also disable fix for zsh 5.0.2

--- shared.zsh
+++ shared.zsh
@@ -1,16 +1,13 @@
 autoload -U select-word-style
 select-word-style bash
-
+setopt SH_WORD_SPLIT
 
 export LANG=ru_RU.UTF-8
-
+export LC_CTYPE=ru_RU.UTF-8
 export GREP_OPTIONS='--color=auto'
 export GREP_COLOR='1;35;40'
 export EDITOR='subl -w -new-window'
 export HOMEBREW_CC="clang"
-
-# Broken in ZSH 5.0.2
-compdef -d nm
 
 precmd () {print -Pn "\e]2; %~/ \a"}
 preexec () {print -Pn "\e]2; %~/ \a"}
